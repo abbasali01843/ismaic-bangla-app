27b22c1a9a8105df472e99e18612b3275abf0caf
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-09 09:48:42 +0600

    Add PrayerTimeDao for Prayer Time database queries

---
+++ app/src/main/java/com/islamic/bangla/data/local/dao/PrayerTimeDao.kt
@@ -0,0 +1,34 @@
+package com.islamic.bangla.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.islamic.bangla.data.model.PrayerTime
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrayerTimeDao {
+    @Insert
+    suspend fun insertPrayerTime(prayerTime: PrayerTime)
+
+    @Insert
+    suspend fun insertAllPrayerTimes(prayerTimeList: List<PrayerTime>)
+
+    @Query("SELECT * FROM prayer_times WHERE city = :city ORDER BY date ASC")
+    fun getPrayerTimesByCity(city: String): Flow<List<PrayerTime>>
+
+    @Query("SELECT * FROM prayer_times WHERE city = :city AND date = :date")
+    fun getTodayPrayerTime(city: String, date: String): Flow<PrayerTime>
+
+    @Query("SELECT * FROM prayer_times ORDER BY date ASC")
+    fun getAllPrayerTimes(): Flow<List<PrayerTime>>
+
+    @Query("SELECT DISTINCT city FROM prayer_times")
+    fun getCities(): Flow<List<String>>
+
+    @Query("DELETE FROM prayer_times WHERE city = :city")
+    suspend fun deletePrayerTimesByCity(city: String)
+
+    @Query("DELETE FROM prayer_times")
+    suspend fun deleteAllPrayerTimes()
+}
