14afc9a8aceda62d6c5c17d34e65f81f22d50280
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-09 09:41:22 +0600

    Add HadithDao for Hadith database queries

---
+++ app/src/main/java/com/islamic/bangla/data/local/dao/HadithDao.kt
@@ -0,0 +1,40 @@
+package com.islamic.bangla.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.islamic.bangla.data.model.Hadith
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface HadithDao {
+    @Insert
+    suspend fun insertHadith(hadith: Hadith)
+
+    @Insert
+    suspend fun insertAllHadiths(hadithList: List<Hadith>)
+
+    @Query("SELECT * FROM hadith ORDER BY collection ASC")
+    fun getAllHadiths(): Flow<List<Hadith>>
+
+    @Query("SELECT * FROM hadith WHERE collection = :collection ORDER BY hadithNumber ASC")
+    fun getHadithsByCollection(collection: String): Flow<List<Hadith>>
+
+    @Query("SELECT * FROM hadith WHERE hadithId = :hadithId")
+    fun getHadithById(hadithId: Int): Flow<Hadith>
+
+    @Query("SELECT * FROM hadith WHERE banglaTranslation LIKE '%' || :searchText || '%' ORDER BY collection ASC")
+    fun searchHadiths(searchText: String): Flow<List<Hadith>>
+
+    @Query("SELECT * FROM hadith WHERE gradeOfAuthenticity = :grade ORDER BY collection ASC")
+    fun getHadithsByGrade(grade: String): Flow<List<Hadith>>
+
+    @Query("SELECT DISTINCT collection FROM hadith")
+    fun getHadithCollections(): Flow<List<String>>
+
+    @Query("DELETE FROM hadith WHERE collection = :collection")
+    suspend fun deleteHadithsByCollection(collection: String)
+
+    @Query("DELETE FROM hadith")
+    suspend fun deleteAllHadiths()
+}
