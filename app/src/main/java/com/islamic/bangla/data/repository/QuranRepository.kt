2ab5611eeef4fcf430d094554c78f8c735a579f8
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-09 09:44:05 +0600

    Add QuranRepository for data access layer

---
+++ app/src/main/java/com/islamic/bangla/data/repository/QuranRepository.kt
@@ -0,0 +1,22 @@
+package com.islamic.bangla.data.repository
+
+import com.islamic.bangla.data.local.dao.QuranDao
+import com.islamic.bangla.data.model.Quran
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class QuranRepository @Inject constructor(
+    private val quranDao: QuranDao
+) {
+    fun getAllSurahs(): Flow<List<Quran>> = quranDao.getAllSurahs()
+
+    fun getSurahById(surahNumber: Int): Flow<Quran> = quranDao.getSurahById(surahNumber)
+
+    fun searchSurahs(searchText: String): Flow<List<Quran>> = quranDao.searchSurahs(searchText)
+
+    suspend fun insertQuran(quran: Quran) = quranDao.insertQuran(quran)
+
+    suspend fun insertAllQuran(quranList: List<Quran>) = quranDao.insertAllQuran(quranList)
+
+    suspend fun deleteAllSurahs() = quranDao.deleteAllSurahs()
+}
