c26bfd7470c0be89ea1b09d8b6c5f73d01f837a3
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-09 09:38:36 +0600

    Add IslamicBanglaApp application class with Hilt

---
+++ app/src/main/java/com/islamic/bangla/IslamicBanglaApp.kt
@@ -0,0 +1,12 @@
+package com.islamic.bangla
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IslamicBanglaApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // App initialization
+    }
+}
